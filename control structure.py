12e05f0dbbc3fd3d7fb3350893bc0c8216ded7c7
codewithSoham <153304259+Sohammolke2233area@users.noreply.github.com>
2025-12-28 13:14:29 +0530

    Add files via upload

---
+++ control structure.py
@@ -0,0 +1,19 @@
+# Task 1: Check if a Number is Even or Odd
+# Problem Statement:  Write a Python program that:
+# 1. 	Takes an integer input from the user.
+# 2. 	Checks whether the number is even or odd using an if-else statement.
+# 3. 	Displays the result accordingly.
+
+# num=int(input("enter the number:"))
+# if num%2==0:
+#     print(f"{num} is even")
+# else:
+#     print(f"{num} is odd")
+#
+
+# Task 2: Sum of Integers from 1 to 50 Using a Loop
+
+total=0
+for i in range(1,51):
+    total+= i
+print(f"The sum of numbers from 1 to 50 is {total}")
